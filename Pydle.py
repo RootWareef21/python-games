9fe16c587fc21d84a9cc1bd301f29700e43e7ce7
wareef <111172564+RootWareef21@users.noreply.github.com>
2026-07-19 11:11:31 +0300

    Update Pydle.py

--- Pydle.py
+++ Pydle.py
@@ -1,3 +1,15 @@
+#0
+#first attempt
+for x in range(5):
+    if x < 3: pydle(x, 0, "", "blue")
+    if x in (0, 2): pydle(x, 1, "", "blue")
+    if x < 2: pydle(x, 2, "", "blue")
+    if x in (2, 4): pydle(x, 2, "", "yellow")
+    if x in (3,): pydle(x, 3, "", "yellow")
+    if x in (2,): pydle(x, 4, "", "yellow")
+for y in range(5):
+    pydle(0, y, "", "blue") 
+    
 #146
 #first attempt
 for x in range(5):
